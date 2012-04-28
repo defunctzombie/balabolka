a3db58be0d103199213718c389181a1c3a539f74
Roman Shtylman <shtylman@gmail.com>
2012-04-28 10:37:36 -0400

    simplify inclusion html down to two lines

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,14 +1,43 @@
 
-var ChatWindow = require('./view/chat_window');
+// host and port for socket.io connection
+var host = 'localhost';
+var port = 8000;
 
-// unique room for domain
-var domain = window.location.hostname;
-var room = io.connect('/' + domain);
+function main() {
+    var ChatWindow = require('./view/chat_window');
 
-var chat_window = new ChatWindow(room);
+    // unique room for domain
+    var domain = window.location.hostname;
+    var room = io.connect('/' + domain, {
+        host: host,
+        port: port
+    });
 
-// connected
-room.on('connect', function() {
-    console.log('connected');
-});
+    var chat_window = new ChatWindow(room);
 
+    // connected
+    room.on('connect', function() {
+        console.log('connected');
+    });
+}
+
+function load_script(url, cb) {
+    var tag = document.createElement('script');
+    tag.async = true;
+    tag.src = url;
+    tag.onload = tag.onreadystatechange = cb;
+    document.getElementsByTagName('head')[0].appendChild(tag);
+}
+
+function check_socketio() {
+    if (window.io) {
+        return main()
+    }
+    load_script('//' + host + ':' + port + '/socket.io/socket.io.js', main);
+}
+
+// detect if jquery is already available
+if (window.jQuery) {
+    check_socketio();
+}
+load_script('//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', check_socketio);
