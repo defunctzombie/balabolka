b3cef47ee3d9538bacbe139cf9428dc1a698a9ac
Roman Shtylman <shtylman@gmail.com>
2012-04-27 22:40:24 -0400

    add bootstrap assets

---
+++ assets/js/chat.js
@@ -0,0 +1,3 @@
+
+// TODO chat window init code
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // 3rd party
 var express = require('express');
 var hbs = require('hbs');
+var jsbundler = require('jsbundler');
 
 var app = express.createServer();
 
@@ -20,10 +21,22 @@ if (kProduction) {
 }
 
 app.use(express.favicon(kPubdir + '/favicon.ico'));
+
+app.use('/js/chat.js', jsbundler.bundle(kPubdir + '/js/chat.js').middleware());
+
 app.use(express.static(kPubdir));
 
 app.get('/', function(req, res, next) {
     res.render('index');
+});
+
+var io = require('socket.io').listen(app);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
 
 if (require.main === module) {
